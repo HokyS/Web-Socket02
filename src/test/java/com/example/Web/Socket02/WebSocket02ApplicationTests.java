0f1b0eedf28166231bc4c793fd8dd845290746a3
Mario Cotumaccio <mariocotumaccio1@gmail.com>
2022-11-10 09:15:30 +0100

    Creating Controllers, DTO and configuration

---
+++ src/test/java/com/example/Web/Socket02/WebSocket02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Web.Socket02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocket02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
